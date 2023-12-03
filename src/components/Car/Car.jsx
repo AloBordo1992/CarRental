208ec4789d919678a14f78eeb2e0e50ce16214cf
AloBordo1992 <122152325+AloBordo1992@users.noreply.github.com>
2023-12-03 14:42:05 +0100

    push

--- src/components/Car/Car.jsx
+++ src/components/Car/Car.jsx
@@ -4,33 +4,19 @@ import { Card, Img } from './Car.style.js';
 
 const Car = ({ imageUrl }) => {
   const imageSource = imageUrl || defaultImage;
-  // -----
-  const marca = 'GMC,';
-  const model = 'FXT';
-  const year = '200';
-  const price = '45';
-  const city = 'Dnipro';
-  const country = 'Ukraine';
-  const typeClass = 'class';
-  const type = 'Suv';
-  const completeSset = 'Yukon Denali';
-  const mileage = '9597';
-  const other = 'Sound system';
-  // -------
+
   return (
     <Card>
       <Img src={imageSource} alt="Car" />
       <div>
         <div>
           <h3>
-            {marca} <span>{model}</span> {year}
+            marca <span>model</span>
           </h3>
-          <p>${price}</p>
+          <p>price</p>
         </div>
         <div>
-          <p>
-            {city} | {country} | {typeClass} |{' '}
-          </p>
+          <p>city | country | typeClass |</p>
           <p>параметри</p>
         </div>
         <button>Learn more</button>
